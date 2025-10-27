75c1b9ba46bfba9651185ac6bbf5aa66dc10e618
Cedric Cannard <ccannard@protonmail.com>
2025-10-27 11:58:35 -0700

    ignore freesurfer files

--- test_freesurfer.m
+++ test_freesurfer.m
@@ -1,12 +1,12 @@
-%% FreeSurfer run from MATLAB on macOS (M1)
-% Subject: UCI29
-% Data folder:
-%   /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/dataset5
-%
-% This script
-% 1. Sets environment variables for FreeSurfer inside MATLAB
-% 2. Converts T1 NIfTI to 001.mgz in $SUBJECTS_DIR/<SUB>/mri/orig
-% 3. Kicks off recon-all (autorecon1 first)
+%% Use FreeSurfer and Fieltrip from MATLAB to generate surface files for 
+% visualization of iEEG electrodes (and more) with the iEEGLAB plugin. 
+% 
+% REQUIREMENTS:
+%   - Install XQuartz: https://www.xquartz.org/
+%   - Install FreeSurfer: https://surfer.nmr.mgh.harvard.edu/fswiki/DownloadAndInstall
+%   - Download/clone Fiedtrip and add path to MATLAB
+% 
+% Author: Cedric Cannard, adapted from Fieldtrip Tutorial. 
 
 clear; close all; clc
 eeglab; close
@@ -72,9 +72,6 @@ cfg.parameter = 'anatomy';
 ft_volumewrite(cfg, mri_acpc);
 
 % 5) Cortical surface extraction with FreeSurfer (Optional)
-% Requirements:
-%   - Install XQuartz: https://www.xquartz.org/
-%   - Install FreeSurfer: https://surfer.nmr.mgh.harvard.edu/fswiki/DownloadAndInstall
 % 
 % Execute FreeSurfer's recon-all functionality from the Linux or 
 % MacOS terminal (Windows via VirtualBox), or from the MATLAB command 
@@ -95,11 +92,40 @@ ft_volumewrite(cfg, mri_acpc);
 fshome     = '/Applications/freesurfer/8.1.0';
 subdir     = SUBJECT_DIR;
 mrfile     = sprintf('%s.nii', cfg.filename);
+% tic
+% system(['export FREESURFER_HOME=' fshome '; ' ...
+% 'source $FREESURFER_HOME/SetUpFreeSurfer.sh; ' ...
+% 'mri_convert -c -oc 0 0 0 ' mrfile ' ' [subdir '/tmp.nii'] '; ' ...
+% 'recon-all -i ' [subdir '/tmp.nii'] ' -s ' 'freesurfer' ' -sd ' subdir ' -all'])
+% toc; gong
+
+% To do minimal surface reconstruction only, using all available CPU cores
+% -1 (optional for speedup)
+% -autorecon1 = preprocessing (motion correction, Talairach, skull strip, etc.)
+% -autorecon2-wm = white matter surface reconstruction (creates surf/lh.white, surf/rh.white)
+% -autorecon3 = pial surface reconstruction and final surfaces (surf/lh.pial, etc.)
+% --> This skips volume segmentation and thickness maps while still 
+% producing surf/* files.
+tic
+nThreads = feature('numcores') - 1;
 system(['export FREESURFER_HOME=' fshome '; ' ...
 'source $FREESURFER_HOME/SetUpFreeSurfer.sh; ' ...
 'mri_convert -c -oc 0 0 0 ' mrfile ' ' [subdir '/tmp.nii'] '; ' ...
-'recon-all -i ' [subdir '/tmp.nii'] ' -s ' 'freesurfer' ' -sd ' subdir ' -all'])
-gong
+'recon-all -i ' [subdir '/tmp.nii'] ' -s freesurfer -sd ' subdir ...
+' -autorecon1 -autorecon2-wm -autorecon3 -openmp ' num2str(nThreads)]);
+toc; gong
+
+%  same but skipping stats/parcellations to save some time
+% nThreads = feature('numcores');  % optional speedup
+tic
+system(['export FREESURFER_HOME=' fshome '; ' ...
+'source $FREESURFER_HOME/SetUpFreeSurfer.sh; ' ...
+'mri_convert -c -oc 0 0 0 ' mrfile ' ' [subdir '/tmp.nii'] '; ' ...
+'recon-all -i ' [subdir '/tmp.nii'] ' -s freesurfer -sd ' subdir ...
+' -autorecon1 -autorecon2-wm -autorecon3 ' ...
+' -noparcstats -noparcstats2 -noparcstats3 -nobalabels ' ...
+' -openmp ' num2str(nThreads)]);
+toc; gong
 
 
 % 6) Import the extracted cortical surfaces into the MATLAB workspace and 
@@ -120,3 +146,97 @@ camlight
 % system(sprintf('mri_info "%s" | head -n 10', t1_mgz));
 % 
 % fprintf('\nDone. Subject folder: %s\n', fullfile(SUBJECT_DIR,SUB_ID));
+
+
+%% Preprocessing of the anatomical CT
+
+
+% 9) Import the anatomical CT into the MATLAB workspace. Similar to the MRI,
+%  the CT scan comes in the format of a single file with an .img or .nii 
+% extension, or a folder containing a series of files with a .dcm or .ima 
+% extension
+ct = ft_read_mri([subjID '_CT_acpc_f.nii']); % we used the dcm series
+
+% 10) In case this cannot be done on the basis of knowledge of the 
+% laterality of electrode implantation, determine the native orientation of
+%  the anatomical CT's left- right axis using ft_determine_coordsys, 
+% similarly to how it was done with the anatomical MRI in Step 3.
+% CRITICAL STEP To correctly fuse the MRI and CT scans at a later step, 
+% accuracy in demarcating the right and left preauricular landmark in the 
+% following step is important for avoiding an otherwise hard to detect flip 
+% of the scan's left and right orientation.
+ct = ft_determine_coordsys(ct);
+
+
+% 11) Align the anatomical CT to the CTF head surface coordinate system by
+% specifying the nasion (at the root of the nose), left and right preauricular 
+% points (just in front of the ear canals), and an interhemispheric location 
+% along the midline at the top of the brain. The CT scan is initially 
+% aligned to the CTF head surface coordinate system given that the ACPC 
+% coordinate system used for the MRI relies on neuroanatomical landmarks 
+% that are not visible in the CT.
+cfg           = [];
+cfg.method    = 'fiducial'; % 'interactive' 'fiducial'
+cfg.coordsys  = 'ctf';
+cfg.viewresult= 'yes';
+ct_ctf = ft_volumerealign(cfg, ct);
+
+
+% 12) Automatically convert the CT's coordinate system into an approximation
+%  of the ACPC coordinate system, the same system the anatomical MRI was 
+% aligned to. The call to ft_hastoolbox ensures that the required SPM 
+% toolbox is on the path.
+ft_hastoolbox('spm12', 1);
+ct_acpc = ft_convert_coordsys(ct_ctf, 'acpc');
+
+%% Fusion of the CT with the MRI
+
+% 8) Import the FreeSurfer-processed MRI into the MATLAB workspace for 
+% later fusion with the CT scan, and specify the coordinate system to which
+% it was aligned.
+fsmri_acpc = ft_read_mri('freesurfer/mri/T1.mgz'); % on Windows, use 'SubjectUCI29_MR_acpc.nii'
+fsmri_acpc.coordsys = 'acpc';
+
+% 13) Fuse the CT with the MRI, a necessary step to link the electrode 
+% locations in the anatomical CT to their corresponding locations in the 
+% anatomical MRI. Given that both scans are from the same subject and their 
+% common denominator is the skull, a rigid body transformation suffices for 
+% their alignment under normal circumstances (the default technique when
+%  using the SPM-method in FieldTrip).
+cfg             = [];
+cfg.method      = 'spm';
+cfg.spmversion  = 'spm12';
+cfg.coordsys    = 'acpc';
+cfg.viewresult  = 'yes';
+ct_acpc_f = ft_volumerealign(cfg, ct_acpc, fsmri_acpc);
+
+% 14) Carefully examine the interactive figure that is produced after the 
+% coregistration is completed, showing the MRI and fused CT superimposed. 
+% A successful fusion will show tight interlocking of CT-positive skull (in blue) and MRI-positive brain and skin tissue (in red).
+% CRITICAL STEP Accuracy of the fusion operation is important for correctly 
+% placing the electrodes in anatomical context in a following step.
+
+
+% 15) Write the MRI-fused anatomical CT out to file
+cfg           = [];
+cfg.filename  = [subjID '_CT_acpc_f'];
+cfg.filetype  = 'nifti';
+cfg.parameter = 'anatomy';
+ft_volumewrite(cfg, ct_acpc_f);
+
+
+%% Electrode placement
+
+% 16) Import the header information from the recording file, if possible. 
+% By giving the electrode labels originating from the header as input to 
+% ft_electrodeplacement in the next step, the labels will appear as a to-do
+%  list during the interactive electrode placement activity. A second 
+% benefit is that the electrode locations can be directly assigned to labels
+%  collected from the recording file, obviating the need to sort and rename
+%  electrodes to match the electrophysiological data.
+
+load([subjID '_hdr.mat']);
+hdr = ft_read_header(hdr);
+
+
+
--- tutorial/seeg_set/freesurfer/scripts/recon-all.cmd
+++ tutorial/seeg_set/freesurfer/scripts/recon-all.cmd
@@ -1,11 +1,11 @@
 \n\n#---------------------------------
-# New invocation of recon-all Mon Oct 27 11:19:30 PDT 2025 
+# New invocation of recon-all Mon Oct 27 11:55:45 PDT 2025 
 \n mri_convert /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/tmp.nii /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig/001.mgz \n
 #--------------------------------------------
-#@# MotionCor Mon Oct 27 11:19:43 PDT 2025
+#@# MotionCor Mon Oct 27 11:55:46 PDT 2025
 \n cp /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig/001.mgz /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/rawavg.mgz \n
 \n mri_info /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/rawavg.mgz \n
 \n mri_convert /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/rawavg.mgz /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig.mgz --conform \n
 \n mri_add_xform_to_header -c /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/transforms/talairach.xfm /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig.mgz /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig.mgz \n
 \n mri_info /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig.mgz \n
-\n mri_synthstrip --threads 1 -i /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig.mgz -o /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/synthstrip.mgz \n
+\n mri_synthstrip --threads 9 -i /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/orig.mgz -o /Users/cedriccannard/Documents/MATLAB/eeglab/plugins/iEEGLAB/tutorial/seeg_set/freesurfer/mri/synthstrip.mgz \n
